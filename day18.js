dcbf2cd7e7eafdb9d060ea0752b1d429fa48534c
zainulla R <152606739+Zainulla1@users.noreply.github.com>
2024-06-27 18:50:08 +0530

    Add files via upload

---
+++ day18.js
@@ -0,0 +1,21 @@
+//Question-1
+
+//What is the output of this function
+
+let count = 0;
+function printCount() {
+    if (count == 0) {
+        let count = 3;
+        console.log("count1", count);
+    }
+    console.log("count2", count);
+}
+printCount();
+
+//Question 2
+
+//What is the output of this function
+
+console.log(2+ '2')
+console.log(2- '2')
+console.log('2'-2)
